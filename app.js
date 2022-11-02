e51fa18cbfb6abc5c2bc00c97b498cec3aa8bff3
PavM9 <p.i.myasnikov@mail.ru>
2022-11-03 02:54:56 +0300

    feat: создать схемы и модели

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+const DATABASE_URL = 'mongodb://localhost:27017/mestodb';
+
+const app = express();
