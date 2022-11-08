c1501c9ec324310e027108a7fa99183e9f8b2fef
PavM9 <p.i.myasnikov@mail.ru>
2022-11-09 01:20:46 +0300

    feat: создать контроллеры и роутеры для пользователей

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { getUsers, getUserById, createUser } = require('../controllers/users');
+
+const users = express.Router();
+
+users.get('/', getUsers);
+users.get('/:userId', getUserById);
+users.post('/', express.json(), createUser);
+// users.patch('/me', express.json(), updateUser);
+// users.patch('/me/avatar', express.json(), updateAvatar);
+
+module.exports = { users };
