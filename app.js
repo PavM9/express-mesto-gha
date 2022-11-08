78c752be6f6dfa30f7fe880b870d4b92e6016470
PavM9 <p.i.myasnikov@mail.ru>
2022-11-09 02:49:29 +0300

    feat: добавить временное решение авторизации

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { routes } = require('./routes');
 
 const { PORT = 3000 } = process.env;
 const DATABASE_URL = 'mongodb://localhost:27017/mestodb';
 
 const app = express();
+
+mongoose
+  .connect(DATABASE_URL)
+  .then(() => {
+    console.log(`Connected to database on ${DATABASE_URL}`);
+  })
+  .catch((err) => {
+    console.log('Error on database connection');
+    console.error(err);
+  });
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '636aea8e727ce278b4b8a33f',
+  };
+
+  next();
+});
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App started on port ${PORT}`);
+});
--- models/user.js
+++ models/user.js
@@ -6,18 +6,24 @@ const userSchema = new mongoose.Schema(
       type: String,
       minlength: 2,
       maxlength: 30,
+      required: true,
       default: 'Жак-Ив Кусто',
     },
     about: {
       type: String,
       minlength: 2,
       maxlength: 30,
+      required: true,
       default: 'Исследователь океана',
     },
     avatar: {
       type: String,
+      required: true,
       default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     },
+  },
+  {
+    versionKey: false,
   },
 );
 
