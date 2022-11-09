4b86d32af527418339c799dff05a0e7a51c71d9e
PavM9 <p.i.myasnikov@mail.ru>
2022-11-09 04:34:53 +0300

    Revert "feat: добавить дополнительные роуты"
    This reverts commit 8b6146aab344777d4ad7502260251c8bf3dab192.

--- controllers/cards.js
+++ controllers/cards.js
@@ -37,46 +37,4 @@ async function deleteCard(req, res) {
   }
 }
 
-async function addLike(req, res) {
-  try {
-    const userId = req.user._id;
-    const card = await Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $addToSet: { likes: userId } },
-      { new: true },
-    );
-
-    if (!card) {
-      const error = new Error('Карточка не найдена');
-      error.name = 'NotFoundError';
-      throw error;
-    }
-
-    res.send(card);
-  } catch (err) {
-    res.status(500).send({ message: err.message });
-  }
-}
-
-async function deleteLike(req, res) {
-  try {
-    const userId = req.user._id;
-    const card = await Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $pull: { likes: userId } },
-      { new: true },
-    );
-
-    if (!card) {
-      const error = new Error('Карточка не найдена');
-      error.name = 'NotFoundError';
-      throw error;
-    }
-
-    res.send(card);
-  } catch (err) {
-    res.status(500).send({ message: err.message });
-  }
-}
-
-module.exports = { getCards, createCard, deleteCard, addLike, deleteLike };
+module.exports = { getCards, createCard, deleteCard };
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { getUsers, getUserById, createUser, updateUser, updateAvatar } = require('../controllers/users');
+const { getUsers, getUserById, createUser } = require('../controllers/users');
 
 const users = express.Router();
 
 users.get('/', getUsers);
 users.get('/:userId', getUserById);
 users.post('/', express.json(), createUser);
-users.patch('/me', express.json(), updateUser);
-users.patch('/me/avatar', express.json(), updateAvatar);
+// users.patch('/me', express.json(), updateUser);
+// users.patch('/me/avatar', express.json(), updateAvatar);
 
 module.exports = { users };
