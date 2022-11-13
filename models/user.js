5dc590ca161fcea8ce9046c446c41881577a46ce
PavM9 <p.i.myasnikov@mail.ru>
2022-11-13 18:20:54 +0300

    feat: добавить поля email и password к схеме пользователя

--- models/user.js
+++ models/user.js
@@ -1,7 +1,21 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema(
   {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
     name: {
       type: String,
       minlength: 2,
